9025685a7b0734a17471ffa39f9f10c16e8a9786
gabriel <gabriel4k2>
2021-04-10 17:31:10 -0300

    Exercise 4 done

--- Chapter3/Exercise4-Copy1.py
+++ Chapter3/Exercise4-Copy1.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
-# In[14]:
+
+# In[22]:
 
 
 import os
 import tarfile
 import html2text
-
+import string
+import re
+from nltk.stem.porter import *
 
 from email.parser import Parser
 from email.policy import default
 from sklearn.base import *
 from sklearn.datasets import load_files
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 dataset_path = "dataset/"
+url_pattern = r'https?://\S+'
+number_pattern= r'\d+(?:\.\d*)?(?:[eE][+-]?\d+)?'
 only_easy_ham = True
 email_parser = None
 htmlconverter = None
+stemmer = None
 
 def extract_bz2(filename, path="."):
     with tarfile.open(filename, "r:bz2") as tar:
@@ -43,12 +51,13 @@ def binarizeLabels(targets):
             
     return targets
 
-'''
-processEmailPayload helper
-Only process text/plain or text/html. Images/gifs or other multimedia are not useful. Multiparts are also not
+"""
+processMessageObj 
+Helper function only process text/plain or text/html. Images/gifs 
+or other multimedia are not useful. Multiparts are also not
 useful because they will be visited later with walk()
 
-'''
+"""
 def processMessageObj(message_object):
     _type = message_object.get_content_type()
     _body = None
@@ -59,12 +68,13 @@ def processMessageObj(message_object):
         #TODO preprocess html
         _body = message_object.get_payload(decode=True)
         _body = _body.decode('latin-1')
-        transformHTMLtoPlain(_body)
+        _body = transformHTMLtoPlain(_body)
 
     return _body
 
 def transformHTMLtoPlain(_str, ignore_images=True):
     global htmlconverter
+
 
     if htmlconverter is None:
         htmlconverter = html2text.HTML2Text()
@@ -72,8 +82,26 @@ def transformHTMLtoPlain(_str, ignore_images=True):
     if ignore_images:
         htmlconverter.ignore_images = True
 
-    htmlconverter.protect_links = True
-    return _str
+    inspect_result = htmlconverter.handle(_str)
+
+    return inspect_result
+
+
+def stemmfy(message):
+    global stemmer
+    _new_body = None
+    if  stemmer is None:
+        stemmer = PorterStemmer()
+
+    for word in message.split():
+        temp = stemmer.stem(word)
+        if _new_body is None:
+            _new_body = temp
+        else:
+            _new_body = _new_body + " " + temp
+
+    return _new_body
+
 
 def getEmailSubject(email_message ):
     global email_parser
@@ -85,15 +113,22 @@ def getEmailSubject(email_message ):
 
     return email_message["subject"]
 
+"""
+Processes the email "payload", that is, the actual message. This processing includes parsing the byte stream
+into an email object, thenn decoding it and finally walking through the "sections"
 
-def processEmailPayload(email_message, to_lower_case ):
+input: email_message = a numpy array element, representing a byte stream
+
+"""
+def processEmailPayload(email_message, to_lower_case, stemm=True, remove_punct = True, substitute_number=True):
     body = ""
     global email_parser
 
     if email_parser is None:
         email_parser = Parser(policy=default)
 
-    email_message = email_parser.parsestr(email_message.decode('iso-8859-1')) #UTF-8 does not work (some email have latin chars
+    email_message = email_parser.parsestr(email_message.decode('iso-8859-1'))
+    #UTF-8 does not work (some email have latin chars)
 
 
 
@@ -105,12 +140,30 @@ def processEmailPayload(email_message, to_lower_case ):
 
     else:
         _temp  =  processMessageObj(email_message)
-        body = body +  processMessageObj(email_message) if _temp is not None else body
+        body = body + _temp if _temp is not None else body
 
 
     if to_lower_case:
         body = body.lower()
-    return body
+
+    #URLS are changed always (hardcoded) it makes no sense to allow them (too much noise)
+    body = re.sub(pattern=url_pattern, repl=' _URL_ ', string=body)
+
+    
+    if substitute_number:
+        body = re.sub(pattern=number_pattern, repl='NUMBER', string=body)
+        
+    if remove_punct:
+        body = body.translate((str.maketrans('', '', string.punctuation)))
+
+
+
+        
+    if stemm:
+        body = stemmfy(body)
+    #If the email message is a html message for instance, the body will be reduced to "none", then simply convert it
+    #to a null string (to not crash further steps)...
+    return body if body is not None else ""
 
 
 
@@ -122,28 +175,28 @@ def convertIndexToString(targets, index_to_string_dict):
     return np.asarray(new_targets )
 
 class PreprocessStrToEmail(BaseEstimator, TransformerMixin):
-    def __init__(self,  subject=True, to_lower_case= False): # no *args or **kargs
-        self.subject = subject
+    def __init__(self,   to_lower_case= False, stemm=True, remove_punct=True,                  substitute_number = True): # no *args or **kargs
+        
         self.to_lower_case = to_lower_case
+        self.stemm = stemm
+        self.remove_punct = remove_punct
+        self.substitute_number =  substitute_number
+
     def fit(self, X, y=None):
         return self # nothing else to do
     def transform(self, X, y=None):
         _parser = Parser(policy=default)
 
-        _X1 = np.array([processEmailPayload(xi, self.to_lower_case ) for xi in X])
-        if self.subject:
-            _X2 =  np.array([getEmailSubject(xi) for xi in X])
-            return np.c_[_X1,_X2]
-            #temp = np.apply_along_axis(func1d=processEmailPayload, axis=0, arr=X)
+        return np.array([processEmailPayload(xi, self.to_lower_case, self.stemm, self.remove_punct                                             ,   self.substitute_number ) for xi in X])
+ 
 
 
-        return _X1
 
 
         
 
 
-# In[2]:
+# In[3]:
 
 
 os.chdir("SpamAssassin")
@@ -152,7 +205,7 @@ for _file in compressed_files:
     extract_bz2(_file, path=dataset_path)
 
 
-# In[3]:
+# In[15]:
 
 
 _dataset = load_files(dataset_path)
@@ -163,14 +216,13 @@ _target_names =  _dataset.target_names
 
 # As we can see below, every data has a categorical label contained in the following set: 
 
-# In[4]:
+# In[16]:
 
 
 print(_target_names)
 
 
-# In[5]:
-
+# In[17]:
 
 
 if only_easy_ham:
@@ -180,7 +232,7 @@ if only_easy_ham:
 
 # It's desirable to simply set the labels as whether they are ham/spam:
 
-# In[6]:
+# In[18]:
 
 
 index_to_str_label = dict()
@@ -194,78 +246,71 @@ _target = binarizeLabels(_target)
 print(_target[:16])
 
 
-# In[7]:
+# As we can see below there is a lot of noise in the message, such as the protocol used which IP sent the message, etc...
+
+# In[19]:
 
 
-print(_data[0])
+print(_data[1])
 
 
-# In[8]:
+# The preprocessing will take care to only get the message proper and the subject if the user wants. The other preprocessing hyperparameters are whether to convert to_lower_case, stemm the words and remove punctuation. Below is a fully processed email message.
+
+# In[23]:
 
 
-headers = Parser(policy=default).parsestr(_data[0].decode('UTF-8'))
+pre_process = PreprocessStrToEmail(to_lower_case=True, stemm=True, remove_punct = True )
+_data_processed_example = pre_process.fit_transform([_data[2]])
 
 
-# In[9]:
+# In[24]:
 
 
-print('To: {}'.format(headers['to']))
+print(_data_processed_example)
 
 
-# In[10]:
+# Below is sightly less preprocessed message. As we can see there is significantly more noise.
+
+# In[25]:
 
 
-print('From: {}'.format(headers['from']))
+pre_process_weaker = PreprocessStrToEmail(to_lower_case=True, stemm=False, remove_punct = True, substitute_number=False)
+_data_processed_example = pre_process_weaker.fit_transform([_data[2]])
 
 
-# In[11]:
+# In[26]:
 
 
-elem = headers.get_payload()
-type(elem)
-print(elem[-1])
+print(_data_processed_example)
 
 
-# In[12]:
+# In[28]:
 
 
-#Let's see the ratio of spam to ham:
-spam_occurrences = np.count_nonzero(_target == 'spam')
-ham_occurrences = np.count_nonzero(_target == 'ham')
-print(spam_occurrences/ham_occurrences)
-
-
-# In[15]:
-
-
-pre_process = PreprocessStrToEmail(to_lower_case=True)
-pre_process.fit_transform(_data)
-_duh = processEmailPayload(_data[0])
-
-
-# In[8]:
-
-
-#Using stratifies shuffle to obtain a roughly similar ratio:
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in sss.split(_data, _target):
-    X_strat_train_set = _data[train_index]
-    X_strat_test_set =  _data[test_index]
-    Y_strat_train_set = _target[train_index]
-    Y_strat_test_set = _target[test_index]
-
-
-# In[10]:
-
-
-#OK, the train set does accurately reflects the data
-spam_occurrences = np.count_nonzero(Y_strat_train_set == 'spam')
-ham_occurrences = np.count_nonzero(Y_strat_train_set == 'ham')
-print(spam_occurrences/ham_occurrences)
+_data_processed = pre_process.fit_transform(_data[0:1000])
 
 
 # In[ ]:
 
+
+
+
+
+# In[29]:
+
+
+count_vect = CountVectorizer()
+
+
+# In[34]:
+
+
+X_train_counts = count_vect.fit_transform(_data_processed)
+
+print("gah")
+
+
+# In[37]:
 
 
 
